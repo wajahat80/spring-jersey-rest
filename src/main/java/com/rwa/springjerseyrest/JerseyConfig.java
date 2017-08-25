fefe92745a3a6d4e889556aa03fa991738bf0490
Raja Ali <raja@codeworx.com.au>
2017-08-25 10:09:58 +1000

    context change

--- src/main/java/com/rwa/springjerseyrest/JerseyConfig.java
+++ src/main/java/com/rwa/springjerseyrest/JerseyConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import com.rwa.springjerseyrest.Controllers.EmployeeController;
 
 @Configuration
-@ApplicationPath("/demo-spring-react/api") //The default mapping path can be changed via javax.ws.rs.ApplicationPath annotation added to ResourceConfig configuration class:
+@ApplicationPath("/spring-jersey-rest/api") //The default mapping path can be changed via javax.ws.rs.ApplicationPath annotation added to ResourceConfig configuration class:
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         register(EmployeeController.class);
